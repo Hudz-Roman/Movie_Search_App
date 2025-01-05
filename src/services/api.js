1274602ec126fed1067f7ef83ed4eccf47ece1e3
Roman Hudz <hudzrom@gmail.com>
2025-01-05 17:27:41 +0200

    deploy .env

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,7 @@ axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 
 const options = {
   headers: {
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Zjc2MWM1M2U1YzlmYWE5NjA0YmNiM2RlNDcwZGMwYSIsIm5iZiI6MTczMjAzMjYwNi4xMjI2OTAyLCJzdWIiOiI2NzNjYjZjMzI0ODViODViM2NhOGUxN2UiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.7GCzCNPwdBjmIrBxU6TY7XJtnXP-4RW8goH2BiS02_Q',
+    Authorization: `Bearer ${import.meta.env.VITE_BEARER_KEY}`,
   },
 };
 
